0c2f9d3fcbe01aeeeb35dba98b625eb97b06fbdf
Vincent Y. <yusyang686@gmail.com>
2026-02-25 11:36:49 -0500

    fixed opencode binary path issue in v1.2.12

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 set -e
-
 mkdir -p ~/.local/share ~/.config
 ln -sf /data ~/.local/share/opencode
 ln -sf /config ~/.config/opencode
-
+# Make sure opencode is in PATH
+export PATH="$HOME/.opencode/bin:$PATH"
 ARGS=""
-
 if [ -n "$OPENCODE_PORT" ]; then
   ARGS="$ARGS --port $OPENCODE_PORT"
 fi
-
 if [ -n "$OPENCODE_HOSTNAME" ]; then
   ARGS="$ARGS --hostname $OPENCODE_HOSTNAME"
 fi
-
 exec opencode $ARGS "$@"
